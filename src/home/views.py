4a77d35053593b9e327cd846eabcd6596b378cca
Tim Krebs <timkrebs9@gmail.com>
2024-09-23 15:17:11 +0200

    Refactor home_page_view to home_view and add about_view
    This commit refactors the home_page_view function to home_view and adds a new about_view function. The home_view function now calls the about_view function. Additionally, the commit creates a new PageVisits object with the path of the request.

--- src/home/views.py
+++ src/home/views.py
@@ -6,15 +6,19 @@ from visits.models import PageVisits
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisits.objects.all()
     qs_page = PageVisits.objects.filter(path=request.path)
     my_title = "Hello there..."
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": qs_page.count(),
         "total_page_visit_count": qs.count(),
     }
-    html_template = "home.html"
-    PageVisits.objects.create()
+
+    PageVisits.objects.create(path=request.path)
     return render(request, html_template, my_context)
