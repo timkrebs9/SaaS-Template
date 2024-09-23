29f3d952fe15329e0cfa2fe9551e6b739d4e0553
Tim Krebs <timkrebs9@gmail.com>
2024-09-23 15:17:20 +0200

    Refactor home_page_view to home_view and add about_view

--- src/home/urls.py
+++ src/home/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page_view
+from .views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_page_view),
+    path('', home_view),
+    path('about/', about_view),
 ]
