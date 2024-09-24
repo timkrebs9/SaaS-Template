1594f6bee426352f7a82a55e28f0adb30a6fc0c5
Tim Krebs <timkrebs9@gmail.com>
2024-09-24 08:07:30 +0200

    Refactor home_view function to calculate and display page visit percentage

--- src/home/views.py
+++ src/home/views.py
@@ -9,16 +9,40 @@ this_dir = pathlib.Path(__file__).resolve().parent
 def home_view(request, *args, **kwargs):
     return about_view(request, *args, **kwargs)
 
+
 def about_view(request, *args, **kwargs):
     qs = PageVisits.objects.all()
-    qs_page = PageVisits.objects.filter(path=request.path)
-    my_title = "Hello there..."
+    page_qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
     html_template = "home.html"
     my_context = {
         "page_title": my_title,
-        "page_visit_count": qs_page.count(),
-        "total_page_visit_count": qs.count(),
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
     }
-
     PageVisits.objects.create(path=request.path)
     return render(request, html_template, my_context)
+
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(**my_context) # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
